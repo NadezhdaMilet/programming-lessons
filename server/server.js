7a12efe4c4fdf02be8c44c9e9395d2fb0d44230a
NadezhdaMilet <nadya.miletenko@bk.ru>
2022-03-03 17:40:14 +0300

    Отдает клиенту на запрос картинку под номером

--- server/server.js
+++ server/server.js
@@ -1,30 +1,77 @@
-const http = require("http");
 const fs = require("fs");
+const http = require("http");
 const requestHandler = (request, response) => {
     response.setHeader("Content-Type", "text/html; charset=utf-8;");
-
     if (request.url === "/home" || request.url === "/") {
         response.write("<h2>Home</h2>");
     } else if (request.url == "/about") {
         response.write("<h2>About</h2>");
     } else if (request.url == "/contact") {
         response.write("<h2>Contacts</h2>");
+    } else if (request.url == "/images/1") {
+        let content = fs.readFileSync("1.jpg");
+        response.writeHead(200, {
+            'Content-Type': 'image/jpeg'
+        })
+        response.end(content);
+
+
+    } else if (request.url == "/images/2") {
+        let content = fs.readFileSync("2.jpg");
+        response.writeHead(200, {
+            'Content-Type': 'image/jpeg'
+        })
+        response.end(content);
+
+
+    } else if (request.url == "/images/3") {
+        let content = fs.readFileSync("3.jpg");
+        response.writeHead(200, {
+            'Content-Type': 'image/jpeg'
+        })
+        response.end(content);
+
+
+    } else if (request.url == "/images/4") {
+        let content = fs.readFileSync("4.jpeg");
+        response.writeHead(200, {
+            'Content-Type': 'image/jpeg'
+        })
+        response.end(content);
+
+
+    } else if (request.url == "/images/5") {
+        let content = fs.readFileSync("5.jpg");
+        response.writeHead(200, {
+            'Content-Type': 'image/jpeg'
+        })
+        response.end(content);
+
+    } else if (request.url == "/images/6") {
+        let content = fs.readFileSync("6.jpg");
+        response.writeHead(200, {
+            'Content-Type': 'image/jpeg'
+        })
+        response.end(content);
+
+    } else if (request.url == "/images/7") {
+        let content = fs.readFileSync("7.jpg");
+        response.writeHead(200, {
+            'Content-Type': 'image/jpeg'
+        })
+        response.end(content);
+
     } else {
         response.write("<h2>Not found</h2>");
     }
-    response.end();
-
     console.log("Url: " + request.url);
     console.log("Тип запроса: " + request.method);
     console.log("User-Agent: " + request.headers["user-agent"]);
-    console.log("Все заголовки");
     console.log(request.headers);
     let Nadya = JSON.stringify(request.headers);
 
-    fs.appendFileSync("hello.txt", request.url);
-    fs.appendFileSync("hello.txt", request.method);
-    fs.appendFileSync("hello.txt", Nadya);
-    console.log("Запись файла завершена. Содержимое файла:");
-
+    fs.appendFileSync("log.txt", request.url);
+    fs.appendFileSync("log.txt", request.method);
+    fs.appendFileSync("log.txt", Nadya);
 };
 http.createServer(requestHandler).listen(3000);
