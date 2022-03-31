6dfa030b142d298ad3ba4da8de6f7650bf161936
NadezhdaMilet <nadya.miletenko@bk.ru>
2022-03-31 09:59:47 +0300

    proverka_str.substring

---
+++ API.js
@@ -0,0 +1,8 @@
+let str = "stringify";
+
+// для substring эти два примера — одинаковы  
+console.log( str.substring(2, 6) ); // "ring" 
+console.log( str.substring(6, 2) ); // "ring" 
+
+// …но не для slice: 
+console.log(str.slice(2, 6)); // "ring" (то же самое) console.log( str.slice(6, 2) ); // "" (пустая строка) 
--- povar.js
+++
@@ -1,7 +0,0 @@
-let str = "Повар! – сказал Повар";
-let target = "Повар";
-
-let pos = -1;
-while ((pos = str.indexOf(target, pos + 1)) != -1) {
-    console.log(pos);
-}
