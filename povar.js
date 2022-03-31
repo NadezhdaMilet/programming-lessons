d8d0ecc7a7e43d6f9a3cce883daef620956255f8
NadezhdaMilet <nadya.miletenko@bk.ru>
2022-03-31 09:51:56 +0300

    proverka_povar2

--- povar.js
+++ povar.js
@@ -1,8 +1,7 @@
-let str = 'Повар! – сказал Повар';
-let target = 'Повар'; // цель поиска 
-let pos = 0;
-while (true) {
-    let foundPos = str.indexOf(target, pos);
-    if (foundPos == -1) break;
-    console.log(`Найдено тут: ${foundPos}`);
-    pos = foundPos + 1; // продолжаем со следующей позиции } 
+let str = "Повар! – сказал Повар";
+let target = "Повар";
+
+let pos = -1;
+while ((pos = str.indexOf(target, pos + 1)) != -1) {
+    console.log(pos);
+}
