2fe7a8f0607c9d0f0d314e483466a68a76776278
NadezhdaMilet <nadya.miletenko@bk.ru>
2022-03-31 10:01:07 +0300

    str.substring

--- API.js
+++ API.js
@@ -5,4 +5,5 @@ console.log( str.substring(2, 6) ); // "ring"
 console.log( str.substring(6, 2) ); // "ring" 
 
 // …но не для slice: 
-console.log(str.slice(2, 6)); // "ring" (то же самое) console.log( str.slice(6, 2) ); // "" (пустая строка) 
+console.log(str.slice(2, 6)); // "ring" (то же самое) 
+console.log( str.slice(6, 2) ); // "" (пустая строка) 
