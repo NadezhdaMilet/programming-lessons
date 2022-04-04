00f3305933292597021b403e32e05fdfa6518486
NadezhdaMilet <nadya.miletenko@bk.ru>
2022-04-04 11:07:34 +0300

    Сохраняет список дел с адреса в JSON файл

--- API.js
+++ API.js
@@ -1,4 +1,20 @@
-let str = "stringify"; 
-// gi, получаем 2 символа, начиная с позиции 4 с конца строки 
-console.log( str.substr(-4, 2) ); 
+const fs = require('fs');
+const axios = require('axios');
+//async function dowloadFile () 
+axios.get("https://jsonplaceholder.typicode.com/todos")
+.then( response => {
+            const Nadya = response.data;
+            console.log(response);
+            const json = JSON.stringify(Nadya);
+            console.log(typeof json); // получение строки
+            console.log(json);
+            fs.writeFile('todos.json', json, function (err) {
+                if (err) return console.log(err);
+                console.log('Привет мир');
+            });
+        }
+    )
+    .catch(function (error) {
+        console.log(error);
+    })
 
