fcdd4f11d0b8321fd08289a73f7783117760576d
NadezhdaMilet <nadya.miletenko@bk.ru>
2022-04-04 12:01:24 +0300

    Сохраняет список дел с адреса в JSON файл 2

--- API.js
+++ API.js
@@ -1,20 +1,24 @@
 const fs = require('fs');
 const axios = require('axios');
-//async function dowloadFile () 
-axios.get("https://jsonplaceholder.typicode.com/todos")
-.then( response => {
-            const Nadya = response.data;
-            console.log(response);
-            const json = JSON.stringify(Nadya);
-            console.log(typeof json); // получение строки
-            console.log(json);
-            fs.writeFile('todos.json', json, function (err) {
-                if (err) return console.log(err);
-                console.log('Привет мир');
-            });
-        }
-    )
-    .catch(function (error) {
-        console.log(error);
-    })
 
+writeJsonplaceholderToFile.then(() => {
+
+ 
+})
+.catch(e => console.error(e));
+
+function writeJsonplaceholderToFile() {
+    return new Promise((resolve, reject) => {
+        axios.get("https://jsonplaceholder.typicode.com/todos")
+            .then(res => {
+                const json = JSON.stringify(res.data);
+                fs.writeFile('todos.json', json, (err) => {
+                    if (err) return reject(err);
+
+
+                    resolve();
+                })
+            })
+            .catch(reject)
+    })
+}
