bd7bce809e02337c76ad1e285fe8b511af1de0d9
NadezhdaMilet <nadya.miletenko@bk.ru>
2022-05-23 11:08:09 +0300

    Proverka  (hello world!)

--- app.js
+++ app.js
@@ -1,37 +1,12 @@
-const express = require('express');
-const app = express();
-app.set('port', process.env.PORT || 3000);
-// пользовательская страница 404
-app.use(function (req, res) {
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 — Не найдено');
-});
-// пользовательская страница 500
-app.use(function (err, req, res, next) {
-console.error(err.stack);
-    res.type('text/plain');
-    res.status(500);
-    res.send('500 — Ошибка сервера');
-});
-app.listen(app.get('port'), function () {
-    console.log('Express запущен на http://localhost:' +
-        app.get('port') + '; нажмите Ctrl+C для завершения.');
-});
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.get('/', function (req, res) {
-    res.type('text/plain');
-    res.send('Главная страница');
-});
-app.get('/about', function (req, res) {
-    res.type('text/plain');
-    res.send('О главной странице');
-});
-// пользовательская страница 404
-app.use(function (req, res, next) {
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 — Не найдено');
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
